33f597f9cbb52781df1ec3c657618fa6be1690f5
Juan X Gomez <xendke@gmail.com>
2020-05-10 11:16:04 -0400

    Small style changes to messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import { Icon, Segment } from 'semantic-ui-react';
+import './Messages.css';
 
-const Messages = (props) => (
-  <Segment.Group>
-    { 
-    	props.messages.map((message) =>
-        (
-          <Segment size='small' key={message.key} textAlign={message.align}>
-            {
-              message.align === "left"
-                ? (
-                  <div>
-                    <Icon size='large' name='user circle' />
-                    {message.data.username || message.data.uid}
-                  </div> 
-                ) : null
-            }
-            { message.data.text }
-            { message.align === "left" ? null : (<Icon size='large' name='chevron left'/>) }
-          </Segment>
+const Messages = (props) => {
+  const messageFromSelf = (message) => (message.align === "left");
+
+  return (
+    <Segment.Group>
+      { 
+        props.messages.map((message) =>
+          (
+            <Segment size='small' key={message.key} textAlign={message.align}>
+              {
+                messageFromSelf(message)
+                  ? (
+                    <div>
+                      <Icon name='user circle' style={{color: 'red'}}/>
+                      <span className="username">{message.data.username || message.data.uid}</span>
+                    </div> 
+                  ) : null
+              }
+              <p className={`${messageFromSelf(message) ? 'message' : 'message-self'}`}>
+                { message.data.text }
+              </p>
+              { messageFromSelf(message) ? null : (<Icon size='large' name='chevron left'/>) }
+            </Segment>
+          )
         )
-      )
-    }
-  </Segment.Group>
-)
+      }
+    </Segment.Group>
+  )
+}
 
 Messages.defaultProps = {
   messages: []
